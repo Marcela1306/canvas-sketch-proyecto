3da5d561eb457c8f8db0b2ea036e3f747f552039
Virginia <agmarcela1306@gmail.com>
2024-12-22 16:28:11 -0600

    sonido

--- Sonido/repo.js
+++ Sonido/repo.js
@@ -2,14 +2,17 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3003; // Cambiado a 3001
+const port = 3003;
 
-app.use(express.static(path.join(__dirname)));
+// Configura la carpeta "public" como estática
+app.use(express.static(path.join(__dirname, 'public')));
 
+// Ruta principal para servir el HTML
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'sonido.html'));
+  res.sendFile(path.join(__dirname, 'public', 'sonido.html'));
 });
 
+// Inicia el servidor
 app.listen(port, () => {
-  console.log(`Servidor escuchando en http://localhost:${port}`);
+  console.log(`Servidor corriendo en http://localhost:${port}`);
 });
