bbf2795166d9fda706d051c81e18d8e71e9021c1
Virginia <agmarcela1306@gmail.com>
2024-12-22 14:29:44 -0600

    Sonido en la web

--- Sonido/repo.js
+++ Sonido/repo.js
@@ -2,12 +2,12 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3002; // Cambiado a 3001
+const port = 3003; // Cambiado a 3001
 
 app.use(express.static(path.join(__dirname)));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'indice.html'));
+  res.sendFile(path.join(__dirname, 'sonido.html'));
 });
 
 app.listen(port, () => {
